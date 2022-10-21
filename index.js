bdaacfd118f525e5978a30c4cc80bff7408be51f
Nilo Vila <eb_nvilaz@bancoripley.com>
2022-10-21 13:17:28 -0500

    commit initial

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hola mundo");
+});
+
+const port = 3000;
+const server = app.listen(port, () => {
+  console.log("listening on port %s.\n", server.address().port);
+});
+
+module.exports = app;
