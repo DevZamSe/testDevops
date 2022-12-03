1ad2b4051b9aa867dcb426807259e0ef443fa313
Nilo Vila <eb_nvilaz@bancoripley.com>
2022-12-03 13:08:38 -0500

    ajuste gdg

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hola mundo");
 });
 
+app.get("/gdg", (req, res) => {
+  res.send("Here we go again!");
+});
+
 const port = 3000;
 const server = app.listen(port, () => {
   console.log("listening on port %s.\n", server.address().port);
